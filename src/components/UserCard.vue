dcba2bafc1ef98f3a512560512165dba4c01df78
Iryna Yehorova <iryna.yehorova@faceit.com.ua>
2022-05-20 17:25:24 +0300

    fix cards margin

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,7 +1,7 @@
 <template>
      <div class="card rounded-3xl bg-white pb-7 sm:w-[250px] md:w-[450px] lg:w-[730px] sm:px-3">
          <div class="grid grid-cols-8 gap-4">
-            <h1 class="col-start-2 col-span-6 card__heading sm:text-base md:text-2xl lg:text-3xl text-black text-center py-5">User #{{item.id ? item.id : Date.now()}}</h1>
+            <h1 class="my-5 col-start-2 col-span-6 card__heading sm:text-base md:text-2xl lg:text-3xl text-black text-center py-5">User #{{item.id ? item.id : Date.now()}}</h1>
             <button @click="deleteUser">
                 <img src="@/assets/trash.svg" />
             </button>
